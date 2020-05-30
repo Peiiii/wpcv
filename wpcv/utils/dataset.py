54eb1c4d6873387e12e18665916a32e51819ad8a
Peiiii <1535376447@qq.com>
2020-05-30 18:36:51 +0800

    m

--- wpcv/utils/dataset.py
+++ wpcv/utils/dataset.py
@@ -1,6 +1,4 @@
-import os, shutil, glob, random
-# from wpkit.fsutil import copy_files_to
-
+import os, shutil, glob, random,time
 def newdir(out_dir):
     if os.path.exists(out_dir): shutil.rmtree(out_dir)
     os.makedirs(out_dir)
@@ -34,6 +32,63 @@ def split_train_val(data_dir, train_dir, val_dir, val_split=0.1, num_val=None, e
     train_files = fs[num_val:]
     copy_files_to(train_files, train_dir)
     copy_files_to(val_files, val_dir)
+
+def split_list(lis,ratios,shuffle=True):
+    if not lis:
+        return None
+    if len(lis)<len(ratios):
+        raise Exception("List is not long enough to be split.")
+    import numpy as np
+    if shuffle:
+        random.shuffle(lis)
+    ratios=np.array(ratios)
+    ratios=ratios/ratios.sum()
+    nums=ratios*len(lis)
+    nums=np.round(nums)
+    total=len(lis)
+    splits=[]
+    current_index=0
+    for i,num in enumerate(nums):
+        end_point=min(current_index+num,total)
+        batch=lis[current_index,end_point]
+        splits.append(batch)
+        current_index=end_point
+    return splits
+
+
+def split_files(src_files,dst_dirs_and_ratios:'{"output":{"train":0.8,"val":0.2}}',shuffle=True,remake_dirs=True):
+    def compress_tree(tree,root=''):
+        leaves = {}
+        def parse_tree(root, dic):
+            for k, v in dic.items():
+                p = os.path.join(root,k)
+                if isinstance(v, dict):
+                    parse_tree(p, v)
+                else:
+                    '''Now it should be a number'''
+                    leaves[p] = v
+        parse_tree(root=root,dic=tree)
+        return leaves
+    tree=compress_tree(dst_dirs_and_ratios)
+    out_dirs=list(tree.keys())
+    ratios=list(tree.values())
+    out_file_lists=split_list(src_files,ratios,shuffle=shuffle)
+    for dir,files in zip(out_dirs,out_file_lists):
+        if remake_dirs and os.path.exists(dir):
+            shutil.rmtree(dir)
+            time.sleep(0.01)
+        print('Coping %s files to %s : %s'%(len(files),dir,files))
+        copy_files_to(files,dir)
+
+def split_dir(src_dir,dst_dirs_and_ratios:'{"output":{"train":0.8,"val":0.2}}',shuffle=True,remake_dirs=True,glob_strings=['*.jpg','*.png']):
+    fs=[]
+    for string in glob_strings:
+        fs+=glob.glob(os.path.join(src_dir,string))
+    split_files(
+        src_files=fs,dst_dirs_and_ratios=dst_dirs_and_ratios,shuffle=shuffle,remake_dirs=remake_dirs
+    )
+    print('Split dir %s finished.'%(src_dir))
+
 
 
 def split_train_val_imagefolder(data_dir, train_dir, val_dir, val_split=0.1, num_val_cls=None, ext='.jpg', shuffle=True,
